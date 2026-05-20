290480a66568d0c1e11bb0c8ccc75e820d804427
Drashtim04 <drashtimaheswari20@gmail.com>
2026-05-20 15:30:47 +0530

    Updated AI Caption Studio

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
+        
         targetCompatibility = JavaVersion.VERSION_11
     }
 
@@ -36,7 +37,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.caption_generator"
+        applicationId = "com.boomit.aicaptionstudio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
